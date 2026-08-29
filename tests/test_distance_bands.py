17cf44b72fc297ee6c56bb7db5e77e0024444e35
Hayden Shin <min.developer.acc@gmail.com>
2026-08-29 17:52:36 +0900

    test: cover requested distance band boundaries

--- tests/test_distance_bands.py
+++ tests/test_distance_bands.py
@@ -7,10 +7,12 @@ from src.ktdb.distance import add_distance_band, derive_distance_band
 
 
 def test_derive_distance_band_uses_half_open_boundaries() -> None:
-    assert derive_distance_band(0) == "under_1km"
-    assert derive_distance_band(1) == "1_to_3km"
-    assert derive_distance_band(3) == "3_to_5km"
-    assert derive_distance_band(20) == "20km_or_more"
+    assert derive_distance_band(0) == "0-2km"
+    assert derive_distance_band(1.999) == "0-2km"
+    assert derive_distance_band(2) == "2-5km"
+    assert derive_distance_band(5) == "5-10km"
+    assert derive_distance_band(10) == "10-20km"
+    assert derive_distance_band(20) == "20km+"
     assert pd.isna(derive_distance_band(pd.NA))
 
 
